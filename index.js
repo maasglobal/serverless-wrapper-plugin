ab5c2bd27a06981c150e2cf571efdc5e61d7a6a7
Hieu Nguyen <blackevil245@gmail.com>
2019-12-10 16:41:24 +0200

    Disable logging

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-'use strict';
+"use strict";
 
-const path = require('path');
-const fs = require('fs-extra');
+const path = require("path");
+const fs = require("fs-extra");
 
-const codeTemplate = require('./lib/wrapped_handler.js');
+const codeTemplate = require("./lib/wrapped_handler.js");
 
-const SAVED_HANDLER_SUFFIX = '__orig__';
-
+const SAVED_HANDLER_SUFFIX = "__orig__";
 
 module.exports = function getPlugin(S) {
-  const SCli = require(S.getServerlessPath('utils/cli'));
+  const SCli = require(S.getServerlessPath("utils/cli"));
 
   class ServerlessWrappper extends S.classes.Plugin {
     static getName() {
@@ -18,23 +17,23 @@ module.exports = function getPlugin(S) {
 
     registerHooks() {
       S.addHook(this.preAction.bind(this), {
-        action: 'codeDeployLambda',
-        event: 'pre',
+        action: "codeDeployLambda",
+        event: "pre"
       });
 
       S.addHook(this.postAction.bind(this), {
-        action: 'codeDeployLambda',
-        event: 'post',
+        action: "codeDeployLambda",
+        event: "post"
       });
 
       S.addHook(this.preAction.bind(this), {
-        action: 'functionRun',
-        event: 'pre',
+        action: "functionRun",
+        event: "pre"
       });
 
       S.addHook(this.postAction.bind(this), {
-        action: 'functionRun',
-        event: 'post',
+        action: "functionRun",
+        event: "post"
       });
 
       return Promise.resolve();
@@ -49,9 +48,11 @@ module.exports = function getPlugin(S) {
      */
     preAction(evt) {
       // Validate: Check Serverless version
-      if (parseInt(S._version.split('.')[1], 10) < 5) {
-        SCli.log('WARNING: This version of the Serverless Wrapper Plugin ' +
-          'will not work with a version of Serverless that is less or greater than than v0.5.x');
+      if (parseInt(S._version.split(".")[1], 10) < 5) {
+        SCli.log(
+          "WARNING: This version of the Serverless Wrapper Plugin " +
+            "will not work with a version of Serverless that is less or greater than than v0.5.x"
+        );
       }
 
       // Get function
@@ -62,23 +63,17 @@ module.exports = function getPlugin(S) {
       const projConfig = project.custom;
 
       if (funcConfig && funcConfig.wrapper === false) {
-          // If wrapper.path exists and is 'false', skip wrapping
-        SCli.log('Wrapper skipped by function-specific config');
+        // If wrapper.path exists and is 'false', skip wrapping
         return Promise.resolve(evt);
-
       } else if (funcConfig && funcConfig.wrapper && funcConfig.wrapper.path) {
-        SCli.log('Using function-specific wrapper file');
         return this._wrapHandler(project, func, evt, funcConfig.wrapper.path);
-
       } else if (projConfig && projConfig.wrapper && projConfig.wrapper.path) {
-        SCli.log('Using project wrapper file');
         return this._wrapHandler(project, func, evt, projConfig.wrapper.path);
       }
 
       // If we can't handle this event, just pass it through
       return Promise.resolve(evt);
     }
-
 
     // Post event
     // ------------------------------------------------------------------------
@@ -95,17 +90,19 @@ module.exports = function getPlugin(S) {
       const funcConfig = func.custom;
       const projConfig = project.custom;
 
-        // No wrapper due to function config override, skip cleanup
+      // No wrapper due to function config override, skip cleanup
       if (funcConfig && funcConfig.wrapper === false) {
         return Promise.resolve(evt);
       }
-        // If we do have a wrapper, we need to clean up intermediate files
-      else if (funcConfig && funcConfig.wrapper && funcConfig.wrapper.path ||
-                 projConfig && projConfig.wrapper && projConfig.wrapper.path) {
+      // If we do have a wrapper, we need to clean up intermediate files
+      else if (
+        (funcConfig && funcConfig.wrapper && funcConfig.wrapper.path) ||
+        (projConfig && projConfig.wrapper && projConfig.wrapper.path)
+      ) {
         const pathSource = path.dirname(func.getFilePath());
         const isLocalRun = !evt.options.pathDist;
         const savedHandlerPath = this._getSavedHandlerPath(func, pathSource);
-				
+
         const action = isLocalRun
           ? fs.move(
               savedHandlerPath,
@@ -131,7 +128,7 @@ module.exports = function getPlugin(S) {
 
       // Get the name of the handler function (within the handler module)
       const handler = func.handler;
-      const handlerFunction = handler.split('.').pop();
+      const handlerFunction = handler.split(".").pop();
 
       // Information about the serverless framework version of the handler
       // [NOTE: the version in the package directory (pathDist)]
@@ -152,8 +149,10 @@ module.exports = function getPlugin(S) {
 
       // Relative path to the wrapper function, from the serverless handler function
       const rootPath = project.getRootPath();
-      const relativeWrapperPath =
-        path.relative(pathSource, path.join(rootPath, wrapperPath));
+      const relativeWrapperPath = path.relative(
+        pathSource,
+        path.join(rootPath, wrapperPath)
+      );
       const absolutePath = path.join(pathSource, relativeWrapperPath);
 
       // 0. Check if file exist in path
@@ -161,18 +160,19 @@ module.exports = function getPlugin(S) {
         if (isLocalRun && tmpHandlerExists) {
           throw new Error(
             `Cannot wrap lambda: Temporary wrapper file found at ${savedHandlerPath}\n` +
-            'It\'s likely that previous run crashed, and left broken state.\n' +
-            'If it\'s the case, then replacing lambda handler file with content of ' +
-            `${savedHandlerFilename}, and removing the ${savedHandlerFilename} should fix the issue.`
+              "It's likely that previous run crashed, and left broken state.\n" +
+              "If it's the case, then replacing lambda handler file with content of " +
+              `${savedHandlerFilename}, and removing the ${savedHandlerFilename} should fix the issue.`
           );
         }
-        return fs.move(serverlessHandlerPath, savedHandlerPath, { overwrite: true })
+        return fs
+          .move(serverlessHandlerPath, savedHandlerPath, { overwrite: true })
           .then(() => {
             // 2. Generate wrapped handler code
             return codeTemplate({
               orig_handler_path: `./${savedHandlerFilename}`,
-              wrapper_path: relativeWrapperPath.replace(/\\/g, '/'), // Support Windows env
-              handler_name: handlerFunction,
+              wrapper_path: relativeWrapperPath.replace(/\\/g, "/"), // Support Windows env
+              handler_name: handlerFunction
             });
           })
           .then(code => {
@@ -181,7 +181,6 @@ module.exports = function getPlugin(S) {
           })
           .then(() => {
             // 4. Resolve the event
-            SCli.log(`Wrapping ${handler} with ${absolutePath}`);
             return evt;
           });
       });
@@ -189,7 +188,7 @@ module.exports = function getPlugin(S) {
 
     // Information about the serverless framework version of the handler
     _getServerlessHandlerPath(serverlessHandler, targetDir) {
-      const serverlessHandlerName = serverlessHandler.split('.')[0];
+      const serverlessHandlerName = serverlessHandler.split(".")[0];
       const serverlessHandlerFilename = `${serverlessHandlerName}.js`;
 
       return path.join(targetDir, serverlessHandlerFilename);
@@ -198,9 +197,9 @@ module.exports = function getPlugin(S) {
     // Information about the serverless framework version of the handler
     _getSavedHandlerFilename(func) {
       const serverlessHandler = func.getHandler();
-      const serverlessHandlerName = serverlessHandler.split('.')[0];
+      const serverlessHandlerName = serverlessHandler.split(".")[0];
 
-        // Information about the saved version of the original serverless handler
+      // Information about the saved version of the original serverless handler
       return `${serverlessHandlerName}${SAVED_HANDLER_SUFFIX}.js`;
     }
 
